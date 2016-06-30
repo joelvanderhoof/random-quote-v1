e745effc700020b4a0decd89107743962073985c
joel vanderhoof <joelvanderhoof@yahoo.com>
2016-06-29 21:54:01 -0700

    update to js/script.js
    -Cleaned up the code
    -Added conditional statements to check for citation and year for quotes
    -moved line 3 to 87

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // event listener to respond to clicks on the page
 // when user clicks anywhere on the page, the "makeQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
 var randomQuote;
@@ -8,8 +7,9 @@ var message = '';
 
 var movieQuotes = [
     //an array of objects which consist of movie quotes and info
+    
     {
-        quote: "Conan what is best \in life? To crush your enemies, see them driven before you, and to hear the lamentation of their women!",
+        quote: "Conan what is best in life? To crush your enemies, see them driven before you, and to hear the lamentation of their women!",
         source: "Conan",
         citation: "Conan the Barbarian",
         year: 1982
@@ -55,27 +55,33 @@ var movieQuotes = [
         source: "Mr. Freeze",
         citation: "Batman and Robin",
         year: 1997
-    },
+    }
 ];
 
-function getRandomQuote () {
+function getRandomQuote() {
     //selects a random quote object from the quotes array
     //returns the randomly selected quote object
-    
-    randomQuoteIndex = Math.floor(Math.random() * (movieQuotes.length - 0)) + 0;
+    "use strict";
+    var randomQuoteIndex = Math.floor(Math.random() * (movieQuotes.length));
     randomQuote = movieQuotes[randomQuoteIndex];
-    return randomQuote[1, 2, 3, 4];
-};
+    return randomQuote;
+}
 
 
 function printQuote(message) {
     //calls getRandomQuote()
     //prints the random quote
-    
+    "use strict";
     getRandomQuote();
-    var pickRandom = randomQuote
-    message += '<p class="quote">' + pickRandom.quote + '</p>';
-    message += '<p class="source">' + pickRandom.source + '<span class="citation"></span>' + pickRandom.citation + '<span class="year">' + pickRandom.year + '</span></p>';
+    message += '<p class="quote">' + randomQuote.quote + '</p>';
+    message += '<p class="source">' + randomQuote.source;
+    if (randomQuote.citation !== undefined) {
+        message += '<span class="citation">' + randomQuote.citation + '</span>';
+    }
+    if (randomQuote.year !== undefined) {
+        message += '<span class="citation">' + randomQuote.year + '</span>';
+    }
     document.getElementById('quote-box').innerHTML = message;
-};
+}
    
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
